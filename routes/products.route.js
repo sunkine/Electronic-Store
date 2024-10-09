5bf6d42ac13a014ac17e0d94c5afebf4f90d95d2
Sympathys <phatanphat@gmail.com>
2024-10-09 21:48:16 +0700

    Them

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,8 +1,16 @@
 import express from "express";
-import getAllProducts from '../controllers/product.class.js'
+import { getAllProducts, createProduct, updateProductByID, deleteProductByID, getProduct } from '../controllers/product.class.js';
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/list-all-product', getAllProducts)
+router.get('/:ID_Product', getProduct);
+
+router.get('/list-all-product', getAllProducts);
+
+router.post('/create-product', createProduct);
+
+router.put('/update-product/:ID_Product', updateProductByID);
+
+router.delete('/delete-product/:ID_Product', deleteProductByID);
 
 export default router;
