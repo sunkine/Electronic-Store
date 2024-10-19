ec82da2a76a24eea684c227dfbeb4a67d46f87fe
Nhan <nextgofori@gmail.com>
2024-10-19 07:59:10 +0700

    dang ky voi xac nhan gui  ve gmail

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { SignIn} from '../controllers/auth.class.js'
+import { SignIn, verifyEmail} from '../controllers/auth.class.js'
 const router = express.Router()
 
 //user sign in
 router.post('/', SignIn)
+router.get('/verify-email/:token', verifyEmail);
 
 export default router;
